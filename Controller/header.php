177561d7845a3841fb99acb35c407c278b3e9a71
Faysal Mahmud <mahmudfaysal64@gmail.com>
2023-07-16 00:28:04 +0600

    hms

---
+++ Controller/header.php
@@ -0,0 +1,5 @@
+<h2>
+<b>Hospital Management System</b>
+<a style = "text-align: left; margin-left:90px" href="../View/home.php">Home</a>
+<a style = "text-align: left; margin-left:10px" href="../Controller/logout.php">Log out</a>
+</h2>
